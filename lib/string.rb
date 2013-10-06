a89762b2168e5a8b87d823fdec1431ad1cea187e
Cole Willsea <coleww@gmail.com>
2013-10-06 14:52:19 -0700

    .haiku? works

--- lib/string.rb
+++ lib/string.rb
@@ -5,5 +5,25 @@ class String
 		self.split(" ").inject(0) { |sum, word|	sum + SYLLABLE_DICTIONARY[word.upcase] }
 	end
 
+	def haiku?
+		return false if self.syllable_count != 17
+
+		words = self.split(" ")
+		count = 0
+		until count == 5
+			count += words.shift.syllable_count
+			return false if count > 5 
+		end
+		until count == 12
+			count += words.shift.syllable_count
+			return false if count > 12 
+		end
+		true
+	end
+
+
+
+
+
 end
 
--- spec/string_spec.rb
+++ spec/string_spec.rb
@@ -1,15 +1,35 @@
 require 'spec_helper'
 
 describe String do
-	it "should know it's #syllable_count" do
-		"alpha".syllable_count.should == 2
+
+	describe ".syllable_count" do
+		it "should know it's count" do
+			"alpha".syllable_count.should == 2
+		end
+
+		it "should know the count of multiple words" do
+			"alpha tango bravo nine".syllable_count.should == 7
+		end
+
+    it "should handle weird white space" do
+      " white      space  ".syllable_count.should == 2
+    end
 	end
 
-	it "should handle multi-word strings" do
-		"alpha tango bravo nine".syllable_count.should == 7
-	end
+  describe ".haiku?" do
+    it "should know that 17 is the number" do
+      "one two three".haiku?.should == false
+    end
 
-	it "should load SYLLABLE_DICTIONARY" do
-		SYLLABLE_DICTIONARY.count.should > 1
-	end
+    it "should know if a string is a haiku" do
+      "this is a haiku written in order to test that i know haiku".haiku?.should == true
+    end
+
+    it "should know that a string is not a haiku" do
+      "i am not a haiku though i am made of seven teen syllables".haiku?.should == false
+    end
+
+  end
+
+
 end

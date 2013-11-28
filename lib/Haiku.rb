9734ac699c25008b2a470c43ceddd0230c4a24bc
Cole Willsea <coleww@gmail.com>
2013-11-28 09:08:17 -0800

    added rate limit

--- lib/Haiku.rb
+++ lib/Haiku.rb
@@ -2,10 +2,21 @@ require_relative '../lib/string.rb'
 require_relative '../lib/syllable_dictionary.rb'
 require_relative '../lib/web_scraper.rb'
 
-TweetStream::Client.new.sample do |status|
+TweetStream::Client.new.sample(language: 'en') do |status|
+	last_tweet_time = Time.now
 	if status.text.haiku?
-		Twitter.retweet(status.id) 
-		puts status.text
+		tweet_time = Time.now
+		if tweet_time - last_tweet_time > 120
+			Twitter.retweet(status.id) 
+			last_tweet_time = tweet_time
+		end
+		File.open("/tweet_log.txt", "a") do |f|
+			f.puts status.text
+			f.puts status
+		end
 	end
-	print "."
 end
+
+
+
+

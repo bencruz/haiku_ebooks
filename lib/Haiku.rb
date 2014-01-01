463e523ee66393a4272c3911dc3eba4e85e1927a
Cole Willsea <coleww@gmail.com>
2014-01-01 12:03:01 -0800

    cleaning up requires

--- lib/Haiku.rb
+++ lib/Haiku.rb
@@ -1,6 +1,6 @@
-require_relative '../lib/string.rb'
-require_relative '../lib/syllable_dictionary.rb'
-require_relative '../lib/web_scraper.rb'
+require_relative 'string.rb'
+require_relative 'syllable_dictionary.rb'
+require_relative 'web_scraper.rb'
 
 class HaikuFinder
   def self.run_bot

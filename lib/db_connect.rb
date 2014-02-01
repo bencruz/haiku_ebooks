ebd45f2bfd26fdec9259228c4b254d9fa796b2f7
Cole Willsea <coleww@gmail.com>
2014-02-01 14:33:34 -0800

    added heroku postgres setup

---
+++ lib/db_connect.rb
@@ -0,0 +1,11 @@
+require 'active_record'
+
+ActiveRecord::Base.establish_connection(ENV['DATABASE_URL'] || 'postgres://localhost/mydb')
+
+class Haiku < ActiveRecord::Base
+  attr_accessible :tweet_id, :author, :body, :posted_at
+
+  validates :tweet_id, :presence => true, :uniqueness => true
+  validates :author, :body, :posted_at, :presence => true
+end
+

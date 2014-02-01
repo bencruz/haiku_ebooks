f6b06f25bcff4342581b55eef0caffdd601bec79
Cole Willsea <coleww@gmail.com>
2014-02-01 14:37:19 -0800

    added tweet saving logic and changed the word status to tweet for consistency

--- lib/Haiku.rb
+++ lib/Haiku.rb
@@ -1,12 +1,13 @@
 require_relative 'string.rb'
 require_relative 'syllable_dictionary.rb'
 require_relative 'web_scraper.rb'
+require_relative 'db_connection.rb'
 
 class HaikuFinder
   def self.run_bot
     begin
-      TweetStream::Client.new.sample(language: 'en') do |status|
-        return status if status.text.haiku?
+      TweetStream::Client.new.sample(language: 'en') do |tweet|
+        return tweet if tweet.text.haiku?
       end
     rescue
       sleep 300
@@ -14,12 +15,28 @@ class HaikuFinder
     end
   end
 
-  def self.post_tweet(status)
-    Twitter.retweet(status.id)
+  def self.post_tweet(tweet)
+    Twitter.retweet(tweet.id)
+  end
+
+  def self.save_tweet(tweet)
+    split_tweet = tweet.text.split(":")
+    text = split_tweet[1]
+    author = split_tweet[0].split("@")[1]
+
+    Haiku.create!(:tweet_id => tweet.id,
+      :author => "@#{author}",
+      :body => text.haikuify,
+      :posted_at => tweet.created_at)
+  end
+
+  def process_tweet(tweet)
+    HaikuFinder.post_tweet(tweet)
+    HaikuFinder.save_tweet(tweet)
   end
 end
 
 loop do
-  HaikuFinder.post_tweet(HaikuFinder.run_bot)
+  HaikuFinder.process_tweet(HaikuFinder.run_bot)
   sleep 1200
 end

555a44af5499c60d7de0bfa1733e0be659634b24
Cole Willsea <coleww@gmail.com>
2014-09-03 19:56:00 -0700

    remove db nonsense is that even on anyrmoe?

--- lib/Haiku.rb
+++ lib/Haiku.rb
@@ -1,7 +1,6 @@
 require_relative 'string.rb'
 require_relative 'syllable_dictionary.rb'
 require_relative 'web_scraper.rb'
-require_relative 'db_connect.rb'
 
 class HaikuFinder
   def self.run_bot
@@ -16,26 +15,13 @@ class HaikuFinder
   end
 
   def self.post_tweet(tweet)
-    Twitter.retweet(tweet.id)
-  end
-
-  def self.save_tweet(tweet)
-    puts tweet.text
-    Haiku.create!(:tweet_id => tweet.id,
-      :author => "@#{tweet.user.screen_name}",
-      :body => tweet.text.haikuify,
-      :posted_at => tweet.created_at)
-  end
-
-  def self.process_tweet(tweet)
     puts "got one!"
-    HaikuFinder.post_tweet(tweet)
-    HaikuFinder.save_tweet(tweet)
+    Twitter.retweet(tweet.id)
   end
 end
 
 loop do
   puts "starting search"
-  HaikuFinder.process_tweet(HaikuFinder.run_bot)
+  HaikuFinder.post_tweet(HaikuFinder.run_bot)
   sleep 1200
 end
